e8e696168d46146700f18c669bf83fd9e5d31fa7
Brian Farias <92887307+BrianBFarias@users.noreply.github.com>
2024-10-23 15:02:12 -0400

    updated app config

--- app.config.js
+++ app.config.js
@@ -1,15 +1,33 @@
 export default () => {
-    
-    return {
-      name: process.env.APP_ENV === 'production' ? 'myRewards' : 'myRewards (DEV)',
-      ios: {
-        bundleIdentifier: process.env.APP_ENV === 'production' ? 'com.my.app' : 'com.anonymous.myRewards',
-      },
-      extra: {
-        eas: {
-          projectId: "e06129e6-3069-486e-8da0-02654828d330"
-        }
-      }
-    };
+    const isProd = process.env.APP_ENV === 'production';
+
+    if(!isProd){
+        return {
+            name: 'myRewards',
+            ios: {
+              bundleIdentifier: isProd ? 'com.my.app' : 'com.anonymous.myRewards',
+            },
+            extra: {
+              eas: {
+                projectId: "e06129e6-3069-486e-8da0-02654828d330"
+              }
+            },
+            owner: "myrewards"
+          };
+
+    }else{
+        return {
+            name: 'myRewards-Prod',
+            ios: {
+              bundleIdentifier: isProd ? 'com.my.app' : 'com.anonymous.myRewards',
+            },
+            extra: {
+              eas: {
+                projectId: "988bf54c-1d84-4d0b-a539-7be1ee48f0dd"
+              }
+            },
+            owner: "myrewards"
+          };
+    }
   };
   
