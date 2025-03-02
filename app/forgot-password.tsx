0148b05469481df3f3727949a43a7ae39f2a6f3f
Landon Zigler <landonzigler@gmail.com>
2025-03-02 17:54:32 -0500

    modified forgot-password.tsx logic

--- app/forgot-password.tsx
+++ app/forgot-password.tsx
@@ -1,120 +1,165 @@
-import { View, Text, TextInput, TouchableOpacity, StyleSheet } from 'react-native';
-import { useEffect, useState } from 'react';
-import { Link } from 'expo-router';
-import { SafeAreaView } from 'react-native-safe-area-context';
-import { router } from 'expo-router';
-
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  StyleSheet,
+} from "react-native";
+import { useEffect, useState } from "react";
+import { Link } from "expo-router";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { router } from "expo-router";
+import { useProps } from "./LoadingProp/propsProvider";
 function ForgotPassword() {
   const [code, setCode] = useState("");
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
-  const textNotificationBox = `We sent a password reset code by email to test*****@ufl.com. Enter it below to reset your password.`
+  const [buttonColor, setButtonColor] = useState("#FBC19F");
+  const textNotificationBox = `We sent a password reset code by email to test*****@ufl.com. Enter it below to reset your password.`;
+  const { alert } = useProps();
+  const getCode = 123456;
 
   useEffect(() => {
-  }, [code, password, confirmPassword]);
+    if (password !== "" && confirmPassword !== "" && code !== "") {
+      setButtonColor("#F98B4E");
+    } else {
+      setButtonColor("#FBC19F");
+    }
+  }, [password, confirmPassword, code]);
 
   const handleResetPassword = () => {
-    // if(password == confirmPassword){
-    //     console.log("Passwords match");
-    // }
-    router.push('/password-reset-success');
+    if (password !== confirmPassword) {
+      alert("", "Passwords do not match", "error");
+      return;
+    } else if (code !== getCode.toString()) {
+      alert("", "Invalid code", "error");
+      return;
+    }
+    /*
+    API logic here to update password for user
+    */
+    router.push("/password-reset-success");
   };
 
   return (
     <View style={styles.container}>
-    <SafeAreaView/>
-      <Text style={styles.title}>Reset password</Text>
+      <SafeAreaView />
+      <View style={styles.topTextContainer}>
+        <Text style={styles.title}>Reset password</Text>
+        <Text style={styles.text}>{textNotificationBox}</Text>
+      </View>
       <View style={styles.formContainer}>
-      <Text style={styles.text}>{textNotificationBox}</Text>
-      <View style={styles.inputBoxContainer}>
-        <Text style={styles.text}>Code</Text>
-        <TextInput
+        <View>
+          <Text style={styles.text}>Code</Text>
+          <TextInput
             style={styles.inputBox}
             value={code}
             onChangeText={setCode}
             placeholder="Enter code"
-        />
-        <Text style={styles.text}>New Password</Text>
-        <TextInput
+          />
+        </View>
+        <View>
+          <Text style={styles.text}>New Password</Text>
+          <TextInput
             style={styles.inputBox}
             value={password}
             onChangeText={setPassword}
             placeholder="Enter new password"
             secureTextEntry
-        />
-        <Text style={styles.text}>Confirm Password</Text>
-        <TextInput
+          />
+        </View>
+        <View>
+          <Text style={styles.text}>Confirm Password</Text>
+          <TextInput
             style={styles.inputBox}
             value={confirmPassword}
             onChangeText={setConfirmPassword}
             placeholder="Confirm new password"
             secureTextEntry
-        />
+          />
+        </View>
       </View>
-      <TouchableOpacity style={styles.button} onPress={handleResetPassword}>
-        <Text style={styles.buttonText}>Reset Password</Text>
-      </TouchableOpacity>
-      <Link href="/sign-in" style={styles.link}>Return to sign in</Link>
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity
+          style={[styles.button, { backgroundColor: buttonColor }]}
+          onPress={handleResetPassword}
+        >
+          <Text style={styles.buttonText}>Reset Password</Text>
+        </TouchableOpacity>
+        <Link href="/sign-in" style={styles.link}>
+          Return to sign in
+        </Link>
       </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        backgroundColor: '#F8F6F1', 
-        height: '100%',
-    },
-    formContainer: {
-        padding: 30,
-    },
-    inputBoxContainer: {
-        marginTop: 20,
-    },
-    title: {
-        marginTop: 40,
-        textAlign: 'center',
-        fontSize: 38,
-        fontWeight: 'bold',
-        fontFamily: 'Avenir Next',
-        color: '#F98B4E',
-    },
-    text: {
-        fontFamily: 'Avenir Next',
-        color: '#7F513A',
-        fontSize: 15,
-        marginBottom: 5,
-    },
-    inputBox: {
-        borderWidth: 1,
-        borderColor: '#7F513A',
-        padding: 10,
-        marginBottom: 15,
-        borderRadius: 10,
-        fontFamily: 'Avenir Next',
-        fontSize: 15,
-        color: '#7F513A'
-    },
-    button: {
-        marginTop: 50,
-        backgroundColor: '#F35E43',
-        padding: 15,
-        borderRadius: 15,
-        alignItems: 'center',
-    },
-    buttonText: {
-        color: '#FFFBF6',
-        fontFamily: 'Avenir Next',
-        fontSize: 20,
-        fontWeight: 'bold',
-    },
-    link: {
-        marginTop: 130,
-        color: '#F98B4E',
-        textAlign: 'center',
-        fontFamily: 'Avenir Next',
-        textDecorationLine: 'underline',
-    },
+  container: {
+    backgroundColor: "#F8F6F1",
+    flex: 1,
+    alignItems: "center",
+    padding: 30,
+  },
+  topTextContainer: {
+    paddingHorizontal: 20,
+  },
+  formContainer: {
+    flex: 1,
+    width: "100%",
+    padding: 20,
+  },
+  title: {
+    marginTop: 40,
+    textAlign: "center",
+    fontSize: 38,
+    fontWeight: "bold",
+    fontFamily: "Avenir Next",
+    color: "#F98B4E",
+  },
+  text: {
+    fontFamily: "Avenir Next",
+    color: "#7F513A",
+    fontSize: 15,
+    marginBottom: 5,
+    padding: 5,
+  },
+  inputBox: {
+    borderWidth: 1,
+    borderColor: "#7F513A",
+    padding: 10,
+    marginBottom: 15,
+    borderRadius: 10,
+    fontFamily: "Avenir Next",
+    fontSize: 15,
+    color: "#7F513A",
+  },
+  buttonContainer: {
+    flex: 1,
+    justifyContent: "center",
+    width: "100%",
+    padding: 20,
+    marginBottom: 36,
+  },
+  button: {
+    marginTop: 50,
+    padding: 15,
+    borderRadius: 15,
+    alignItems: "center",
+  },
+  buttonText: {
+    color: "#FFFBF6",
+    fontFamily: "Avenir Next",
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+  link: {
+    marginTop: 50,
+    color: "#F98B4E",
+    textAlign: "center",
+    fontFamily: "Avenir Next",
+    textDecorationLine: "underline",
+  },
 });
 
 export default ForgotPassword;
--- app/password-reset-success.tsx
+++ app/password-reset-success.tsx
@@ -1,65 +1,67 @@
-import React from 'react'
-import { View, Text, StyleSheet, TouchableOpacity} from 'react-native'
-import { SafeAreaView } from 'react-native-safe-area-context'
-import { SvgXml } from 'react-native-svg';
-import { checkmark } from '@/assets/images/MR-logos';
-import {router } from 'expo-router'
+import React from "react";
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { SvgXml } from "react-native-svg";
+import { checkmark } from "@/assets/images/MR-logos";
+import { router } from "expo-router";
 
 function passwordResetSuccess() {
   return (
     <View style={styles.container}>
-        <SafeAreaView/>
-       <View style={styles.checkmarkContainer}>
-        <SvgXml xml={checkmark}/>
-       </View>
-       <View style={styles.textContainer}>
-         <Text style={styles.text}>Password reset success!</Text>
-       </View>
-       <View style={styles.buttonContainer}>
-         <TouchableOpacity style={styles.button} onPress={() => router.push('/')}>
-            <Text style={styles.buttonText}>return to sign in</Text>
+      <SafeAreaView />
+      <View style={styles.checkmarkContainer}>
+        <SvgXml xml={checkmark} />
+      </View>
+      <View style={styles.textContainer}>
+        <Text style={styles.text}>Password reset success!</Text>
+      </View>
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => router.push("/")}
+        >
+          <Text style={styles.buttonText}>return to sign in</Text>
         </TouchableOpacity>
-       </View>
+      </View>
     </View>
-  )
+  );
 }
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        alignItems: 'center',
-        backgroundColor: '#F8F6F1', 
-        height: '100%',
-        padding: 30,
-    },
-    checkmarkContainer: {
-        marginTop: 150,
-    },
-    textContainer: {
-        marginTop: 30,
-    },
-    text: {
-        fontFamily: 'Avenir Next',
-        color: '#F98B4E',
-        fontSize: 38,
-        fontWeight: 'bold',
-        textAlign: 'center',
-    },
-    buttonContainer: {
-        marginTop: 20,
-        width: '100%',
-    },
-    button: {
-        marginTop: 50,
-        backgroundColor: '#F35E43',
-        padding: 15,
-        borderRadius: 15,
-        alignItems: 'center',
-    },
-    buttonText: {
-        color: '#FFFBF6',
-        fontFamily: 'Avenir Next',
-        fontSize: 20,
-        fontWeight: 'bold',
-    }
-})
-export default passwordResetSuccess
+  container: {
+    flex: 1,
+    alignItems: "center",
+    backgroundColor: "#F8F6F1",
+    padding: 30,
+  },
+  checkmarkContainer: {
+    marginTop: 150,
+  },
+  textContainer: {
+    marginTop: 30,
+  },
+  text: {
+    fontFamily: "Avenir Next",
+    color: "#F98B4E",
+    fontSize: 38,
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+  buttonContainer: {
+    marginTop: 20,
+    width: "100%",
+  },
+  button: {
+    marginTop: 50,
+    backgroundColor: "#F35E43",
+    padding: 15,
+    borderRadius: 15,
+    alignItems: "center",
+  },
+  buttonText: {
+    color: "#FFFBF6",
+    fontFamily: "Avenir Next",
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+});
+export default passwordResetSuccess;
