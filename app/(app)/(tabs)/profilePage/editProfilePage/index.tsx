f44aff16665fbb6698afb77545f331ff1170a4b0
Landon Zigler <landonzigler@gmail.com>
2025-03-20 16:21:50 -0400

    added async/await for api calls

--- app/(app)/(tabs)/profilePage/editProfilePage/index.tsx
+++ app/(app)/(tabs)/profilePage/editProfilePage/index.tsx
@@ -1,5 +1,14 @@
 import React, { useEffect, useState } from "react";
-import { Text, View, StyleSheet, Pressable, TextInput, Dimensions, Image, TouchableOpacity } from "react-native";
+import {
+  Text,
+  View,
+  StyleSheet,
+  Pressable,
+  TextInput,
+  Dimensions,
+  Image,
+  TouchableOpacity,
+} from "react-native";
 import { useRouter } from "expo-router";
 import { color_pallete } from "@/constants/Colors";
 import formatDate from "@/services/formatDate";
@@ -10,125 +19,149 @@ export default function EditProfilePage() {
   const [firstName, setFirstName] = useState("");
   const [lastName, setLastName] = useState("");
   const [buttonColor, setButtonColor] = useState("#FBC19F");
-  const {profile, fetchProfile} = localData();
+  const { profile, fetchProfile } = localData();
 
   useEffect(() => {
-    if (firstName !== "" || lastName !== ""){
+    if (firstName !== "" || lastName !== "") {
       setButtonColor("#F98B4E");
     } else {
       setButtonColor("#FBC19F");
     }
   }, [firstName, lastName]);
 
-  const handleSaveChanges = () => {
+  const handleSaveChanges = async () => {
     if (firstName !== "" || lastName !== "") {
-       updateUser({
+      await updateUser({
         fullname: {
           firstName,
-          lastName, 
+          lastName,
         },
       });
       fetchProfile();
-
       router.back();
     }
   };
 
-    return (
-      <View style={[styles.container, { height: Dimensions.get("window").height - 90 }]}>
-        <View style={styles.content}>
-          <View style={styles.topSection}>
-            {/* User Icon */}
-             <Image
-                source={{
-                  uri: "https://hebbkx1anhila5yf.public.blob.vercel-storage.com/image-LdlKUMY8VdtdQfwRogRVi4MU4LuzvX.png",
-                  }}
-                style={styles.userIcon}
-               />
-            {/* User Info */}
-            <Text style={styles.userName}>{`${profile?.fullname?.firstName} ${profile?.fullname?.lastName}`}</Text>
-            <View style={styles.emailWrapper}>
-              <View style={styles.emailContainer}>
-                <View style={styles.emailLine} />
-                <Text style={styles.emailText}>{profile?.email}</Text>
-                <View style={styles.emailLine} />
-              </View>
-            </View>
-  
-            {/* Membership Info */}
-            <View style={styles.membershipContainer}>
-              <View style={styles.membershipRow}>
-                <Text style={styles.membershipText}>Valued MyRewards member since:</Text>
-                <Text style={styles.membershipText}>
-                {formatDate(profile?.date_created ? new Date(profile?.date_created) : new Date())}
-                </Text>
-              </View>
-              <View style={styles.membershipRow}>
-                <Text style={styles.membershipText}>Birthday:</Text>
-                <Text style={styles.membershipText}>{formatDate(profile?.birthdate ? new Date(profile.birthdate) : new Date())}</Text>
-              </View>
+  return (
+    <View
+      style={[
+        styles.container,
+        { height: Dimensions.get("window").height - 90 },
+      ]}
+    >
+      <View style={styles.content}>
+        <View style={styles.topSection}>
+          {/* User Icon */}
+          <Image
+            source={{
+              uri: "https://hebbkx1anhila5yf.public.blob.vercel-storage.com/image-LdlKUMY8VdtdQfwRogRVi4MU4LuzvX.png",
+            }}
+            style={styles.userIcon}
+          />
+          {/* User Info */}
+          <Text
+            style={styles.userName}
+          >{`${profile?.fullname?.firstName} ${profile?.fullname?.lastName}`}</Text>
+          <View style={styles.emailWrapper}>
+            <View style={styles.emailContainer}>
+              <View style={styles.emailLine} />
+              <Text style={styles.emailText}>{profile?.email}</Text>
+              <View style={styles.emailLine} />
             </View>
           </View>
-  
-          {/* Editable Fields */}
-          <View style={styles.inputContainer}>
-            <Text style={styles.inputLabel}>Edit first name</Text>
-            <TextInput
-              value={firstName}
-              onChangeText={setFirstName}
-              style={styles.inputField}
-              placeholder="Firstname"
-            />
-  
-            <Text style={styles.inputLabel}>Edit last name</Text>
-            <TextInput
-              value={lastName}
-              onChangeText={setLastName}
-              style={styles.inputField}
-              placeholder="Lastname"
-            />
-          </View>
-  
-          {/* Reset Password Button */}
-          <TouchableOpacity style={styles.resetPasswordButton} onPress={() => router.navigate('profilePage/editProfilePage/verify-email')}>
-            <Text style={styles.resetPasswordText}>reset password</Text>
-          </TouchableOpacity>
-  
-          {/* Action Buttons */}
-          <View style={styles.actionButtonsContainer}>
-            <Pressable onPress={() => router.back()} style={styles.cancelButton}>
-              <Text style={styles.cancelButtonText}>cancel</Text>
-            </Pressable>
-            <Pressable style={[styles.saveChangesButton, { backgroundColor: buttonColor }]} onPress={handleSaveChanges}>
-              <Text style={styles.saveChangesText}>save changes</Text>
-            </Pressable>
+
+          {/* Membership Info */}
+          <View style={styles.membershipContainer}>
+            <View style={styles.membershipRow}>
+              <Text style={styles.membershipText}>
+                Valued MyRewards member since:
+              </Text>
+              <Text style={styles.membershipText}>
+                {formatDate(
+                  profile?.date_created
+                    ? new Date(profile?.date_created)
+                    : new Date()
+                )}
+              </Text>
+            </View>
+            <View style={styles.membershipRow}>
+              <Text style={styles.membershipText}>Birthday:</Text>
+              <Text style={styles.membershipText}>
+                {formatDate(
+                  profile?.birthdate ? new Date(profile.birthdate) : new Date()
+                )}
+              </Text>
+            </View>
           </View>
         </View>
+
+        {/* Editable Fields */}
+        <View style={styles.inputContainer}>
+          <Text style={styles.inputLabel}>Edit first name</Text>
+          <TextInput
+            value={firstName}
+            onChangeText={setFirstName}
+            style={styles.inputField}
+            placeholder="Firstname"
+          />
+
+          <Text style={styles.inputLabel}>Edit last name</Text>
+          <TextInput
+            value={lastName}
+            onChangeText={setLastName}
+            style={styles.inputField}
+            placeholder="Lastname"
+          />
+        </View>
+
+        {/* Reset Password Button */}
+        <TouchableOpacity
+          style={styles.resetPasswordButton}
+          onPress={() =>
+            router.navigate("profilePage/editProfilePage/verify-email")
+          }
+        >
+          <Text style={styles.resetPasswordText}>reset password</Text>
+        </TouchableOpacity>
+
+        {/* Action Buttons */}
+        <View style={styles.actionButtonsContainer}>
+          <Pressable onPress={() => router.back()} style={styles.cancelButton}>
+            <Text style={styles.cancelButtonText}>cancel</Text>
+          </Pressable>
+          <Pressable
+            style={[styles.saveChangesButton, { backgroundColor: buttonColor }]}
+            onPress={handleSaveChanges}
+          >
+            <Text style={styles.saveChangesText}>save changes</Text>
+          </Pressable>
+        </View>
       </View>
-    );
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-   header:{
-      backgroundColor:color_pallete[10],
-      elevation: 0,
-      shadowOpacity: 0.1,
-      borderBottomWidth:2,
-      shadowColor:'black',
-      shadowRadius:3,
-      shadowOffset:{
-        height:5,
-        width:0
-      },
-      borderBottomColor:color_pallete[2],
+  header: {
+    backgroundColor: color_pallete[10],
+    elevation: 0,
+    shadowOpacity: 0.1,
+    borderBottomWidth: 2,
+    shadowColor: "black",
+    shadowRadius: 3,
+    shadowOffset: {
+      height: 5,
+      width: 0,
     },
-    headerText:{
-      fontSize: 30,
-      fontWeight: 'bold',
-      fontFamily:'Avenir Next',
-      color:color_pallete[2],
-      marginLeft:'5%'
-    },
+    borderBottomColor: color_pallete[2],
+  },
+  headerText: {
+    fontSize: 30,
+    fontWeight: "bold",
+    fontFamily: "Avenir Next",
+    color: color_pallete[2],
+    marginLeft: "5%",
+  },
   container: {
     flex: 1,
     backgroundColor: "#fffbf7",
@@ -198,7 +231,7 @@ const styles = StyleSheet.create({
     marginBottom: 24,
     flexDirection: "column",
     justifyContent: "center",
-    alignSelf: "center"
+    alignSelf: "center",
   },
   inputLabel: {
     fontFamily: "AvenirNext-Regular",
@@ -225,7 +258,7 @@ const styles = StyleSheet.create({
     padding: 12,
     borderRadius: 12,
     marginBottom: 24,
-    alignSelf:  "center"
+    alignSelf: "center",
   },
   resetPasswordText: {
     fontFamily: "AvenirNext-Bold",
@@ -249,7 +282,7 @@ const styles = StyleSheet.create({
     paddingVertical: 12,
     marginRight: 10,
     borderRadius: 12,
-    alignItems: "center"
+    alignItems: "center",
   },
   cancelButtonText: {
     fontFamily: "AvenirNext-Medium",
@@ -262,7 +295,7 @@ const styles = StyleSheet.create({
     paddingVertical: 12,
     marginLeft: 10,
     borderRadius: 12,
-    alignItems: "center"
+    alignItems: "center",
   },
   saveChangesText: {
     fontFamily: "AvenirNext-Bold",
